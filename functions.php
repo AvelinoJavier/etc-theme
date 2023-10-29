e039f31a102857de31cbf955a8b065296f4fda83
Avelino Javier Florentín Agüero <javierflorentin_90@hotmail.com>
2023-10-29 01:03:20 -0300

    header ready

--- functions.php
+++ functions.php
@@ -1,16 +1,38 @@
 <?php
+/**
+ * Theme Functions
+ *
+ * This file (functions.php) contains custom functions and styles for the etc-theme.
+ * It enqueues styles and scripts, and customizes the theme using the WordPress Customizer.
+ *
+ * @package etc-theme
+ */
 
-// style and scripts
-add_action('wp_enqueue_scripts', 'bootscore_child_enqueue_styles');
+$bootscore_child_version = '1.0';
+
+/** Style and scripts */
 function bootscore_child_enqueue_styles() {
+	global $bootscore_child_version;
 
-  // style.css
-  wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+	// style.css .
+	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css', false, $bootscore_child_version );
 
-  // Compiled main.css
-  $modified_bootscoreChildCss = date('YmdHi', filemtime(get_stylesheet_directory() . '/css/main.css'));
-  wp_enqueue_style('main', get_stylesheet_directory_uri() . '/css/main.css', array('parent-style'), $modified_bootscoreChildCss);
+	// Compiled main.css .
+	$modified_bootscore_child_css = gmdate( 'YmdHi', filemtime( get_stylesheet_directory() . '/css/main.css' ) );
+	wp_enqueue_style( 'main', get_stylesheet_directory_uri() . '/css/main.css', array( 'parent-style' ), $modified_bootscore_child_css );
 
-  // custom.js
-  wp_enqueue_script('custom-js', get_stylesheet_directory_uri() . '/js/custom.js', false, '', true);
+	// custom.js .
+	wp_enqueue_script( 'custom-js', get_stylesheet_directory_uri() . '/js/custom.js', false, $bootscore_child_version, true );
 }
+add_action( 'wp_enqueue_scripts', 'bootscore_child_enqueue_styles' );
+
+/**
+ * Callback function for removing the icon upload field from theme customizer
+ *
+ * @param WP_Customize_Manager $wp_customize The Customizer manager instance.
+ *                             This parameter provides access to customize theme settings.
+ */
+function bootscore_child_remove_site_icon_control( $wp_customize ) {
+	$wp_customize->remove_control( 'site_icon' );
+}
+add_action( 'customize_register', 'bootscore_child_remove_site_icon_control' );
--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,26 @@
 jQuery(function ($) {
+	const $navbar = $('.navbar');
+	const initialNavbarHtml = $('.navbar').html();
 
-    // Do stuff here
+	let navbarChanged = false;
 
-}); // jQuery End
+	function centerBrandLogo() {
+		let $navbarMenuItems = $('.navbar-nav .menu-item');
+		let $navbarMenuItemsLenght = $navbarMenuItems.length;
+
+		if ($navbarMenuItemsLenght % 2 === 0 && window.matchMedia('(min-width: 992px)').matches) {
+			let middleIndex = Math.floor($navbarMenuItemsLenght / 2);
+			$navbarMenuItems.eq(middleIndex - 1).after($('.navbar-brand'));
+
+			navbarChanged = true;
+		}
+		else if (navbarChanged) {
+			$navbar.html(initialNavbarHtml);
+
+			navbarChanged = false;
+		}
+	}
+	centerBrandLogo();
+
+	$(window).resize(centerBrandLogo);
+});
